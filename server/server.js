91472719b046e9534af49447d6614cb4703b0ad3
Lanxiang Wang <lanxiang.wang13@gmail.com>
2018-01-19 16:26:58 -0500

    Setup socket.io

--- server/server.js
+++ server/server.js
@@ -1,25 +1,40 @@
 // built-in module
 const path = require('path');
+const http = require('http');
+const publicPath = path.join(__dirname, '../public');
 
 // third-party module
 const express = require('express');
+const socketIO = require('socket.io');
 
 // setup for heroku
 const port = process.env.PORT || 3000;
 
 // setup
 var app = express();
+var server = http.createServer(app);
+var io = socketIO(server);
 
 // serve static resources
-const publicPath = path.join(__dirname, '../public');
+// middleware
 app.use(express.static(publicPath));
 
+// io.on register an event listener, when this even happens, server can do something
+// connection is a default event
+io.on('connection', (socket) => {
+  console.log('New user connected');
+
+  socket.on('disconnect', () => {
+    console.log('User disconnected from server.');
+  })
+});
 
 
 
 
 
-app.listen(port, () => {
+
+server.listen(port, () => {
   console.log(`Server is up on ${port}`);
 })
 
